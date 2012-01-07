8025329f5630387c822e5ee5e426f85b5e0d5df8
Jonathan Ballet <jon@multani.info>
2012-01-07 13:54:00 +0800

    Add some comments and clarification on my convert script

--- wiki-trac-to-rst.py
+++ wiki-trac-to-rst.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
+"""Roughly convert from Trac Wiki syntax to RestructuredText"""
 
 from pprint import pprint as pp
 import re
 import sys
 
 
-patterns = [r"^"+ ("=" * i) + " (.*) " + ("=" * i)
-            for i in range(1, 10)]
-re_titles = [re.compile(p, re.MULTILINE) for p in patterns]
+title_patterns = [r"^"+ ("=" * i) + " (.*) " + ("=" * i)
+                  for i in range(1, 10)]
+re_titles = [re.compile(p, re.MULTILINE) for p in title_patterns]
 
 re_inlines = [
-    (re.compile(r), repl)
-    for r, repl in [
+    (re.compile(reg), replace_by)
+    for reg, replace_by in [
         (r"'''(.*?)'''", r"*\1*"),
         (r"''(.*?)''", r"**\1**"),
     ]]
 
 re_link = re.compile(r'\[([\S]+) ([^\]]+)\]')
 re_code_block = re.compile(r'{{{(.*?)}}}', re.DOTALL)
-
 re_code_highlight = re.compile(r'.*#!(\w+)')
+
 
 def run(path):
     with open(path) as fr, open("%s.rst" % path.replace('.txt', ''), "w") as fw:
-        process_fps(fr, fw)
+        process_file(fr, fw)
 
-
-def process_fps(r, w):
+def process_file(r, w):
     text = r.read()
 
     text = replace_weird_trac_directives(text)
@@ -36,7 +36,6 @@ def process_fps(r, w):
     text = replace_code_block(text)
 
     w.write(text)
-    #print text
 
 
 def replace_weird_trac_directives(text):
@@ -44,6 +43,7 @@ def replace_weird_trac_directives(text):
 
 
 def replace_inlines(text):
+    """Replace inline syntax"""
     for r, repl in re_inlines:
         text = r.sub(repl, text)
     return text
@@ -57,21 +57,19 @@ def replace_code_block(text):
             return "``%s``" % m
 
         if m.startswith(' '):
-            indent = m
+            # Don't indent if it already looks indented
+            code = m
         else:
-            indent = "    %s" % "\n    ".join(
+            code = "    %s" % "\n    ".join(
                 l for l in m.split("\n"))
 
-        #code = "\n::\n%s\n\n" % indent
-        code = indent
         code = re_code_highlight.sub(r'\n.. code-block:: \1\n', code)
 
         if "::" not in code:
+            # Previous regex didn't apply, manually set code block
             code = "\n::\n%s\n\n" % code
 
         return code
-
-
 
     return re_code_block.sub(repl, text)
 
@@ -91,41 +89,44 @@ def replace_links(text):
                 return "-" + s.lower()
 
             link = link.replace('/', '')
+            # Split the Wiki name on capitalized characters
             r = re.sub(r"([%s])" % ("".join(chr(c) for c in range(65, 91))),
                        split_caps,
                        link)
 
             return ":doc:`%s`" % r.lstrip('-')
 
-        links_replacement = [
+        # Those are Wiki links but inserted with the full URL in the source
+        # Try to be smart (but it's difficult -_- )
+        urls_replacement = [
             ('http://divmod.org/trac/wiki/DivmodCombinator',
-             ':ref:`combinator`'),
+             ':doc:`products/combinator`'),
             ('http://divmod.org/trac/wiki/DivmodNevow/Athena',
-             ':ref:`athena`'),
+             ':doc:`products/nevow/athena`'),
             ('http://divmod.org/trac/wiki/DivmodNevow/AthenaTesting',
-             ':ref:`athena-testing`'),
+             ':doc:`products/nevow/athena-testing`'),
             ('http://divmod.org/trac/wiki/DivmodNevow/Deployment/ReverseProxy',
-             ':ref:`nevow-deployement-reverse-proxy`'),
+             ':doc:`products/nevow/reverse-proxy`'),
             ('http://divmod.org/trac/wiki/PeopleUsingDivmod',
-             ':ref:`people-using-divmod`'),
+             ':doc:`people-using-divmod`'),
             ('http://divmod.org/trac/wiki/SoftwareReleases/Nevow',
-             ':ref:`software-releases-nevow`'),
+             ':doc:`software-releases-nevow`'),
             ('http://divmod.org/trac/wiki/UsingVertex',
-             ':ref:`using-vertex`'),
+             ':doc:`using-vertex`'),
         ]
 
-        for l, r in links_replacement:
-            if link == l:
-                return r
+        for url, url_replace in urls_replacement:
+            if link == url:
+                return url_replace
 
         else:
+            # It looks like a normal, extern link, just process it as it.
             return "`%s <%s>`_" % (text, link)
 
     return re_link.sub(repl, text)
 
 
 def replace_titles(text):
-
     for i, (r, motif) in enumerate(
         zip(re_titles, ["=", "=", "'", '"', "~", "_", "-"])):
         def repl(match):
@@ -144,7 +145,6 @@ def replace_titles(text):
         text = r.sub(repl, text)
 
     return text
-
 
 
 if __name__ == '__main__':
